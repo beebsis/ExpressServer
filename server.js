985b8666391dca17d13e7bb5452ed1029985ebf2
Beebsis <65894260+beebsis@users.noreply.github.com>
2022-11-29 15:26:29 +0100

    API, Auth, register, user etc

--- server.js
+++ server.js
@@ -3,26 +3,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
+const { allowedNodeEnvironmentFlags } = require('process');
 const PORT = process.env.PORT || 3500;
 
 //  Custom logger middleware
 app.use(logger);
 
 //  CORS APPLIANCE
-//  Cross Origin Resource Sharing
-const whitelist = ['https//www.da-beebs.com', 'http://127.0.0.1:5500', 'http://localhost:3500'/*, 'https://www.google.com'*/];
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionSuccessStatus: 200
-};
 app.use(cors(corsOptions));
 
 //  Built-in middleware to handle url-encoded data
@@ -35,49 +25,13 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 // Built-in middleware for static files
-app.use(express.static(path.join(__dirname, '/public')));
+app.use('/', express.static(path.join(__dirname, '/public')));
 
-
-
-//Route handlers
-app.get('^/$|/index(.html)?', (req, res) => {
-    //bad mojo
-    // res.sendFile('./views/index.html', { root: __dirname });
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, '/new-page.html'); //default 302 - want a 301
-});
-
-app.get('/nobody-home(.html)?', (req, res, next) => {
-    console.log('Attempted to reach - but nobody was home. Sorry.');
-    next();
-}, (req, res) => {
-    res.send('NOBODY IS HOME GOD DAMMIT');
-});
-
-// chaining route handlers
-const one = (req, res, next) => {
-    console.log('one');
-    next();
-}
-
-const two = (req, res, next) => {
-    console.log('two');
-    next();
-}
-
-const three = (req, res) => {
-    console.log('three');
-    res.send('Finished!');
-}
-
-app.get('/chain(.html)?', [one, two, three]);
+//Routes from route dir & subdir
+app.use('/', require('./routes/root'));
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
+app.use('/employees', require('./routes/api/employees'));
 
 // Grabbing non-sites
 app.all('/*', (req, res) => {
